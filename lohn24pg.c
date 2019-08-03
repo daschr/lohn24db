d2ada49f77f03dfd7101c34c59d0afe24604e8e3
David Schramm <david.schramm@live.com>
2019-08-03 21:00:22 +0200

    escaping '

--- lohn24pg.c
+++ lohn24pg.c
@@ -40,13 +40,17 @@ int auth_callback (const struct _u_request * request, struct _u_response * respo
 		ulfius_set_string_body_response(response, 401, "unauthorized\n");
 		return U_CALLBACK_CONTINUE;
 	}
-	if(!is_alphanum(request->auth_basic_user) || !is_alphanum(request->auth_basic_password)){
+	/*if(!is_alphanum(request->auth_basic_user) || !is_alphanum(request->auth_basic_password)){
 		ulfius_set_string_body_response(response, 401, "nice try ;)\n");
 		return U_CALLBACK_CONTINUE;
 	}
 	printf("attempt with %s | %s\n",request->auth_basic_user, request->auth_basic_password);
-
-	if(check_password((const char *) request->auth_basic_user,(const char *)request->auth_basic_password))
+	*/
+	char esc_user[512],esc_pw[512];
+	str_repl(esc_user,512,request->auth_basic_user,"'","\\'");
+	str_repl(esc_pw,512,request->auth_basic_password,"'","\\'");
+	
+	if(check_password((const char *) esc_user,(const char *) esc_pw))
 		ulfius_set_string_body_response(response, 200, "authorized\n");
 	else
 		ulfius_set_string_body_response(response, 401, "unauthorized\n");
